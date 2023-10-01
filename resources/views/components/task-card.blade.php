b13c5508515355675c2425181726b93c277c7a93
Amil Silahic <jinobie02@gmail.com>
2023-10-01 16:23:46 -0600

    adding tailwind

--- resources/views/components/task-card.blade.php
+++ resources/views/components/task-card.blade.php
@@ -1,3 +1,5 @@
-<div>
-    <h1>{{$tasks->title}}</h1>
+<div class="bg-red-500 p-4 mb-2">
+    <h1 class="text-xl">{{$tasks->title}}</h1>
+    <span>{{$tasks->due_date}}</span>
+    <p>{{$tasks->description}}</p>
 </div>
