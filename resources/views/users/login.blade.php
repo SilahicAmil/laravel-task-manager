c856acf121320218056d1de0ff75aa4674da9331
Amil Silahic <jinobie02@gmail.com>
2023-10-31 21:46:56 -0600

    idk

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -6,7 +6,6 @@
 
         <form method="POST" action="/users/authenticate">
             @csrf
-
             <div class="mb-6">
                 <label for="email" class="inline-block text-lg mb-2">Email</label>
                 <label>
